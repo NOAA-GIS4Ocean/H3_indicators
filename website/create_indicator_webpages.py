723255340705922c4e1e6fe5fab5b5cdaaf556d8
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2024-10-03 14:25:11 -0400

    Create action to generate webpages

---
+++ website/create_indicator_webpages.py
@@ -0,0 +1,18 @@
+import geopandas as gpd
+import numpy as np
+import os
+
+for i in range(1,7):
+  url = "https://raw.githubusercontent.com/NOAA-GIS4Ocean/H3_indicators/main/data/indicators_all_res{}.geojson".format(i)
+
+  gdf = gpd.read_file(url)
+  gdf["log10(n)"] = np.log10(gdf["n"])
+
+  m = gdf.explore(
+     column = "log10(n)"
+     )
+
+  filename = url.split("/")[-1].replace(".geojson",".html")
+  fout = os.path.join("deploy", filename)
+
+  m.save(fout)
